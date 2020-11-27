298855fe084d5419a8bfbdf525aadca40072331c
machrus44 <3103119127@stematel.smktelkon-pwt.sch.id>
2020-11-27 21:52:57 +0700

    first commit

---
+++ tambah_menu.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Haykal Machrus</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"><script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div>
+        <div class="loader"></div>
+    </div>
+
+    <div class="medsos">
+        <div class="container">
+            <ul>
+                <li><a href="https://www.youtube.com/channel/UCLS1d22oPZgxjX8tkdzb6Qg/featured?view_as=subscriber"><i class="fab fa-youtube"></i></a></li>
+                <li><a href="https://www.instagram.com/hykl_03/"><i class="fab fa-instagram"></i></i></a></li>
+            </ul>
+        </div>
+        
+    </div>
+    <header>
+        <div class="container">
+            <h1><a href="index.html">Cafe 04</a></h1>
+            <ul>
+                <li><a href="index.html">HOME</a></li>
+                <li><a href="about.html">FOOD</a></li>
+                <li><a href="service.html">DRINK</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+                <li class="active"><a href="tambah_menu.php">TAMBAH</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <section class="label">
+        <div class="container">
+            <p>Home / Tambah Menu</p>
+        </div>
+    </section>
+
+    <section class="service">
+        <div class="container">
+            <button><a href="index.php">Tambah Menu</a></button>
+            <br/><br/>
+
+            <form action="add.php" method="post" name="form1">
+                <table width="25%" border="0">
+                </table>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
